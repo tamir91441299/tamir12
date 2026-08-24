416cf84bf3718c011a17211904bddacd1edc0b6b
tamir91441299 <311968769+tamir91441299@users.noreply.github.com>
2026-08-24 17:26:42 +0800

    feat: update My Hero Academia episode video URLs
    Replace placeholder video URLs with functional Google Drive links for episodes 2 through 5.

--- src/data/anime/myHeroAcademia.ts
+++ src/data/anime/myHeroAcademia.ts
@@ -38,73 +38,73 @@ export const MY_HERO_ACADEMIA_S1: Movie = {
       episodeNumber: 2,
       title: '2-р анги - Баатар болохын тулд (What It Takes to Be a Hero)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1iVKu4knukOvSvVGDsY5LuEPrdViviJZD/view?usp=drivesdk'
     },
     {
       episodeNumber: 3,
       title: '3-р анги - Хүчирхэг булчин (Roaring Muscles)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1oHcvzo1kcbwpqV__dduxgZaSs_gnE8EV/view?usp=drivesdk'
     },
     {
       episodeNumber: 4,
       title: '4-р анги - Эхлэлийн шугам (Start Line)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1jjSBOPsIqXEZHSIF5sd5aaSN-FKi0K8h/view?usp=drivesdk'
     },
     {
       episodeNumber: 5,
       title: '5-р анги - Миний чадвар (What I Can Do For Now)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1lkye7Ooy6qjPuGfDRA_ugsJtr7ijIkh3/view?usp=drivesdk'
     },
     {
       episodeNumber: 6,
       title: '6-р анги - Уур хилэнгийн тэмцэл (Rage, You Damned Nerd)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1Yiw53fhOze1axyJbXBFUP0eVi8xiKjqV/view?usp=drivesdk'
     },
     {
       episodeNumber: 7,
       title: '7-р анги - Дэкү vs Каччан (Deku vs. Kacchan)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1qBtj7rlUSg8pI1rL6Fe64l1rU6JfvrYy/view?usp=drivesdk'
     },
     {
       episodeNumber: 8,
       title: '8-р анги - Бакугогийн эхлэлийн цэг (Bakugo\'s Start Line)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1EcjzJh_Y-p7FfxD5R4RsAA4iUbFasw43/view?usp=drivesdk'
     },
     {
       episodeNumber: 9,
       title: '9-р анги - Хичээгээрэй Ийда (Yeah, Just Do Your Best, Iida!)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackSeeTheWorld.mp4'
+      videoUrl: 'https://drive.google.com/file/d/10gRVvR_dVZ_seD4spInvdmFsAo2OA8Tp/view?usp=drivesdk'
     },
     {
       episodeNumber: 10,
       title: '10-р анги - Үл мэдэгдэх хүчний дайралт (Encounter with the Unknown)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1UBVIA2klzF1l6DMkpseGEZtxggkpHZRP/view?usp=drivesdk'
     },
     {
       episodeNumber: 11,
       title: '11-р анги - Тоглоом дууслаа (Game Over)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1v7P8zTyUDa_gybVpmzEfmQn-rl3uC9D5/view?usp=drivesdk'
     },
     {
       episodeNumber: 12,
       title: '12-р анги - Бүхний Дээд (All Might)',
       duration: '24 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4'
+      videoUrl: 'https://drive.google.com/file/d/1s9nrCSBZ9RyzpXXOWV7-UL1qcf5sgJ9_/view?usp=drivesdk'
     },
     {
       episodeNumber: 13,
       title: '13-р анги - Хүн бүрийн сэтгэлд (In Each of Our Hearts - Төгсгөл)',
       duration: '25 мин',
-      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+      videoUrl: 'https://drive.google.com/file/d/132zF0IW3NCRCyi_JM36IRamRrAbVT4r0/view?usp=drivesdk'
     }
   ]
 };
